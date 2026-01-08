5fdcbc4b792b7fc1c07c68235a8e97127791bd81
V5.xie <45307429+xiehuijie@users.noreply.github.com>
2026-01-08 08:02:25 +0000

    feat: 更新 Prettier 配置，添加 js-yaml 依赖；启用 tsup 的代码拆分功能

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts', 'src/cli.ts'],
   format: ['esm', 'cjs'],
   dts: true,
-  splitting: false,
+  splitting: true,
   sourcemap: false,
   clean: true,
   shims: true,
